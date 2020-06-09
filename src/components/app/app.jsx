98f8b0cf8bc82d8dd2494fdd5968dd77bac545ec
Roman Gertsvolf <roman.gertsvolf@gmail.com>
2020-06-09 14:06:09 +0300

    module1-task1

---
+++ src/components/app/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
+
+
+const App = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const {gameTime, errorCount} = props;
+
+  return <WelcomeScreen
+    time={gameTime}
+    errorCount={errorCount}
+  />;
+};
+
+export default App;
